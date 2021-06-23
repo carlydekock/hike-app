5606b9d256e2c7aaca06065337505a64bbd059dd
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-06-22 17:59:13 -0700

    info successfully being grabbed from form

--- client/src/components/AddHike.jsx
+++ client/src/components/AddHike.jsx
@@ -1,37 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const AddHike = () => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [length, setLength] = useState("");
+  const [elevation, setElevation] = useState("");
+  const [time, setTime] = useState("");
+  const [keywords, setKeywords] = useState("");
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+
+  const handleSubmit = (e) => {
+    console.log('inside submit');
+    e.preventDefault();
+  
+  }
   return (
     <div className="mb-4 p-2">
       <form action="">
         <div className="row mb-2">
           <div className="col">
-            <input type="text" className="form-control" placeholder="Name of Hike" />
+            <input value={name} onChange={(e) => setName(e.target.value)} name="name" type="text" className="form-control" placeholder="Name of Hike" />
           </div>
           <div className="col">
-            <input type="text" className="form-control" placeholder="Description of Hike" />
+            <input value={description} onChange={(e) => setDescription(e.target.value)} name="description" type="text" className="form-control" placeholder="Description of Hike" />
           </div>
         </div>
         <div className="row mb-2">
           <div className="form-group col-md-4">
-            <input type="text" className="form-control" id="inputLength" placeholder="Length (in miles)" />
+            <input value={length} onChange={(e) => setLength(e.target.value)} name="length" type="text" className="form-control" placeholder="Length (in miles)" />
           </div>
           <div className="form-group col-md-4">
-            <input type="text" className="form-control" id="inputElevation" placeholder="Elevation (in feet)" />
+            <input value={elevation} onChange={(e) => setElevation(e.target.value)}name="elevation" type="text" className="form-control" placeholder="Elevation (in feet)" />
           </div>
           <div className="form-group col-md-4">
-            <input type="text" className="form-control" id="inputTime" placeholder="Time to Complete" />
+            <input value={time} onChange={(e) => setTime(e.target.value)}name="time" type="text" className="form-control" placeholder="Time to Complete" />
           </div>
         </div>
         <div className="row mb-2">
           <div className="form-group col-md-4">
-            <input type="text" className="form-control" id="inputKeywords" placeholder="Keywords" />
+            <input value={keywords} onChange={(e) => setKeywords(e.target.value)} name="keywords" type="text" className="form-control" placeholder="Keywords" />
           </div>
           <div className="form-group col-md-4">
-            <input type="text" className="form-control" id="inputLatitude" placeholder="Latitude" />
+            <input value={latitude} onChange={(e) => setLatitude(e.target.value)} name="latitude" type="text" className="form-control" placeholder="Latitude" />
           </div>
           <div className="form-group col-md-4">
-            <input type="text" className="form-control" id="inputLongitude" placeholder="Longitude" />
+            <input value={longitude} onChange={(e) => setLongitude(e.target.value)} name="longitude" type="text" className="form-control" placeholder="Longitude" />
           </div>
           <div className="col">
             <select className="custom-select my-1 mr-sm-2">
@@ -42,7 +56,7 @@ const AddHike = () => {
               <option value="wildlife">Wildlife</option>
             </select>
           </div>
-          <button className="btn btn-primary btn-sm">Add</button>
+          <button type="submit" onClick={handleSubmit} className="btn btn-primary btn-sm">Add</button>
         </div>
       </form>
     </div>
