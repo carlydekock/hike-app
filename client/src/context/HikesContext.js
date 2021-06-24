8b215d6dbed50e13ba78fa5787bf5e278bc99321
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-06-23 19:10:42 -0700

    selected hike info pulling through on detail page

--- client/src/context/HikesContext.js
+++ client/src/context/HikesContext.js
@@ -4,12 +4,13 @@ export const HikesContext = createContext();
 
 export const HikesContextProvider = props => {
   const [hikes, setHikes] = useState([]); 
+  const [selectedHike, setSelectedHike] = useState(null);
 
   const addHike = (hike) => {
     setHikes([...hikes, hike]);
   }
   return (
-    <HikesContext.Provider value={{hikes, setHikes, addHike}}>
+    <HikesContext.Provider value={{hikes, setHikes, addHike, selectedHike, setSelectedHike}}>
       {props.children}
     </HikesContext.Provider>
   );
--- client/src/routes/Detail.jsx
+++ client/src/routes/Detail.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { HikesContext } from '../context/HikesContext';
 
 const Detail = () => {
+  const { id } = useParams();
+  const { selectedHike, setSelectedHike } = useContext(HikesContext);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await HikesContext.get(`/${id}`);
+        setSelectedHike(response.data.data.hike);
+      } catch(err) {
+          console.log(err);
+      }
+    };
+    fetchData(); // eslint-disable-next-line
+  }, []);
+  
   return (
     <div>
-      Detail
+      {selectedHike && selectedHike.name}
     </div>
   )
 };
