73959f0427a60a2506794152ffecadc951f4dd9a
CARLYDEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-08-10 21:56:38 -0700

    cleaning up auth test for protected route

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -4,32 +4,30 @@ import AddHike from '../components/AddHike';
 import HikesList from '../components/HikesList';
 import NavBar from '../components/NavBar';
 import Image from '../components/Image';
-import { useAuth0 } from '@auth0/auth0-react';
-const axios = require('axios');
 
 const Home = () => {
-  const { getAccessTokenSilently } = useAuth0();
 
-  async function callProtectedApi(){
-    try {
-      const token = await getAccessTokenSilently();
-      const response = await axios.get('http://localhost:3000/protected', {
-        headers: {
-          authorization: `Bearer ${token}`
-        }
-      })
-      console.log(response.data);
-    } catch (err){
-      console.log(err.message);
-    }
-  }
+  // //Troubleshooting authentication
+  // async function callProtectedApi(){
+  //   try {
+  //     const token = await getAccessTokenSilently();
+  //     const response = await axios.get('http://localhost:3000/protected', {
+  //       headers: {
+  //         authorization: `Bearer ${token}`
+  //       }
+  //     })
+  //     console.log(response.data);
+  //   } catch (err){
+  //     console.log(err.message);
+  //   }
+  // }
 
   return (
     <div>
       <NavBar />
       <Header />
       <Image />
-        <button type="button" className="btn btn-primary" onClick={callProtectedApi}>Call Protected API</button>
+        {/* <button type="button" className="btn btn-primary" onClick={callProtectedApi}>Call Protected API</button> */}
       <AddHike />
       <HikesList />
     </div>
