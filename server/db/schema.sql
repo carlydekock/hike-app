a202e6e8382f8db9ba93ec305d05dd757ca93d9d
CARLYDEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-08-10 18:47:57 -0700

    Updated hikes_list with user_id, added notes about troubleshooting trigger setup

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,5 +1,16 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  auth_id VARCHAR(255) UNIQUE,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  email_address VARCHAR(255) UNIQUE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE TABLE hikes_list (
   id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL,
   name VARCHAR(255) NOT NULL,
   description TEXT NOT NULL,
   length VARCHAR(255) NOT NULL,
@@ -9,7 +20,8 @@ CREATE TABLE hikes_list (
   latitude DECIMAL NOT NULL,
   longitude DECIMAL NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
 CREATE TABLE trip_reports (
@@ -26,13 +38,45 @@ CREATE TABLE trip_reports (
   FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  auth_id VARCHAR(255) UNIQUE,
-  first_name VARCHAR(255),
-  last_name VARCHAR(255),
-  email_address VARCHAR(255) UNIQUE,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
 
+
+-- Troubleshooting creating trigger 
+
+-- CREATE OR REPLACE FUNCTION log_updated_time()
+-- RETURNS TRIGGER
+-- LANGUAGE PLPGSQL
+-- AS $$
+-- BEGIN 
+--   new.updated_at = TIMESTAMP
+-- END;
+-- $$
+
+-- delimiter $$
+-- CREATE TRIGGER update_timestamp
+-- AFTER UPDATE 
+-- ON users
+-- FOR EACH ROW
+-- EXECUTE PROCEDURE log_updated_time();
+-- $$
+
+-- CREATE TABLE users_history (
+--   id SERIAL PRIMARY KEY,
+--   user_id INT NOT NULL,
+--   changed_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--   FOREIGN KEY (user_id) REFRENCES users (id)
+-- );
+
+-- CREATE TABLE trip_reports_history (
+--   id SERIAL PRIMARY KEY,
+--   hike_id INT NOT NULL,
+--   user_id INT NOT NULL,
+--   changed_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--   FOREIGN KEY (hike_id) REFERENCES hikes_list (id),
+--   FOREIGN KEY (user_id) REFERENCES users (id)
+-- );
+
+-- CREATE TABLE hikes_list_history (
+--   id SERIAL PRIMARY KEY,
+--   hike_id INT NOT NULL,
+--   changed_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
