951dd3fc540dd694b8a153feaeedd0f3df06f572
CARLYDEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-08-10 21:52:54 -0700

    added bearer token to headers for home route

--- client/src/components/HikesList.jsx
+++ client/src/components/HikesList.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useContext } from 'react';
 import HikeFinder from '../apis/HikeFinder';
 import { HikesContext } from '../context/HikesContext';
 import { useHistory } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const HikesList = (props) => {
   const { hikes, setHikes } = useContext(HikesContext);
+  const { getAccessTokenSilently} = useAuth0();
   //history represents history of the browser
   let history = useHistory();
 
@@ -12,7 +14,12 @@ const HikesList = (props) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await HikeFinder.get("/");
+        const token = await getAccessTokenSilently();
+        const response = await HikeFinder.get("/", {
+          headers: {
+            authorization: `Bearer ${token}`
+          }
+        });
         setHikes(response.data.data.hikes);
         console.log(response);
       } catch (err) {
@@ -22,6 +29,7 @@ const HikesList = (props) => {
 
     fetchData(); // eslint-disable-next-line
   }, []);
+
 
   const handleDelete = async (e, id) => {
     //this stops from going up to the table row, where it will interfere with the onClick for details
