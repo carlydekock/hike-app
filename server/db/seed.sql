67c5d3f5aaa2dcc2dc19b8dea2b6de3d0df0b519
CARLYDEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-08-10 18:48:59 -0700

    updated users seed data to include auth_id, hikes_list seed data needs to be updated still for user_id

--- server/db/seed.sql
+++ server/db/seed.sql
@@ -1,3 +1,5 @@
+INSERT INTO users (auth_id, first_name, last_name, email_address) VALUES ('auth0|60d68c7dc0b80a006a831d43', 'Carly', 'Dekock', 'carly.m.dekock@gmail.com');
+
 INSERT INTO hikes_list (name, description, length, elevation_gain, time, keywords, latitude, longitude) VALUES ('Mount Adams South Climb', 'The south climb or lunch counter approach.', '12.0 miles', 6700, '6 hours', 'wildflowers, vista', 46.1359, -121.4976);
 
 INSERT INTO hikes_list (name, description, length, elevation_gain, time, keywords, latitude, longitude) VALUES ('Mount Baker Heliotrope Ridge', 'Heliotrope ridge trail overlooking Mount Baker', '5.5 miles', 1400, '3 hours', 'wildflowers, vista, mountain view', 48.8021, -121.8957);
@@ -9,8 +11,6 @@ INSERT INTO hikes_list (name, description, length, elevation_gain, time, keyword
 INSERT INTO hikes_list (name, description, length, elevation_gain, time, keywords, latitude, longitude) VALUES ('Diablo Lake', 'Overlook beautiful blue lake in the North Cascades.', '7.6 miles', 1400, '3 hours', 'vista, lake', 48.7206, -121.1216);
 
 INSERT INTO hikes_list (name, description, length, elevation_gain, time, keywords, latitude, longitude) VALUES ('Blue Lake', 'Towering granite peaks, forests, meadows and wildflowers with a pristine blue mountain lake.', '4.4 miles', 1050, '3 hours', 'lake, forest', 48.5191, -120.6742);
-
-INSERT INTO users (first_name, last_name, email_address) VALUES ('Carly', 'Dekock', 'carly.m.dekock@gmail.com');
 
 INSERT INTO trip_reports (hike_id, user_id, name, title, description, hiked_at) VALUES ('1', '1', 'Carly', 'Sunny day!', 'Great hike on a sunny day the other day, conditions good.', 'Jun 24');
 
