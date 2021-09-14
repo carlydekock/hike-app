f7afd1a375f71a14862460477ae2af2a2147a209
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-09-13 21:07:03 -0700

    setting user id in state in home route

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import AddHike from '../components/AddHike';
 import HikesList from '../components/HikesList';
 import NavBar from '../components/NavBar';
 import Image from '../components/Image';
+import { useAuth0 } from "@auth0/auth0-react";
+import HikeFinder from '../apis/HikeFinder';
 
 const Home = () => {
+
+  // console.log('this is state in home', this.state);
+  // const user = useAuth0();
+  // console.log('this is user on home page', user);
+  const { getAccessTokenSilently} = useAuth0();
+  const [userId, setUserId] = useState('');
+
+  useEffect(() => {
+    const fetchId = async () => {
+      try {
+        const token = await getAccessTokenSilently();
+        const response = await HikeFinder.get("/", {
+          headers: {
+            authorization: `Bearer ${token}`
+          }
+        });
+        console.log('this is inside home route', response);
+        setUserId(response.data.data.user);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchId(); // eslint-disable-next-line
+  }, []);
+  
 
   // //Troubleshooting authentication
   // async function callProtectedApi(){
@@ -28,7 +56,7 @@ const Home = () => {
       <Header />
       <Image />
         {/* <button type="button" className="btn btn-primary" onClick={callProtectedApi}>Call Protected API</button> */}
-      <AddHike />
+      <AddHike userId={userId}/>
       <HikesList />
     </div>
   )
