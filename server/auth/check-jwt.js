04da51e2be4f7a6b7e8eaa8ca705cf48b51380ff
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-09-18 22:18:42 -0700

    fixed extra spacing errors

--- server/auth/check-jwt.js
+++ server/auth/check-jwt.js
@@ -6,18 +6,18 @@ const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
 
 const checkJwt = jwt({
-    secret: jwks.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: `https://${domain}/.well-known/jwks.json`,
-    }),
-    audience: audience,
-    issuer: `https://${domain}/`,
-    algorithms: ['RS256']
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${domain}/.well-known/jwks.json`,
+  }),
+  audience: audience,
+  issuer: `https://${domain}/`,
+  algorithms: ['RS256'],
 });
 
 
 module.exports = {
-    checkJwt
+  checkJwt,
 };
