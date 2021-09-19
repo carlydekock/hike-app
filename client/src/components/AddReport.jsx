102de3204f120873a8b8f521b9dc181c95ac51bc
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-09-18 17:26:18 -0700

    cleaned up bootstrap styling on form

--- client/src/components/AddReport.jsx
+++ client/src/components/AddReport.jsx
@@ -31,32 +31,33 @@ const AddReport = () => {
   }
 
   return (
-    <div className="mb-2 pb-2">
-      <form action="">
-        <div className="row pb-2">
-          <div className="form-group col-4">
-            <label htmlFor="name">Name</label>
-            <input value={name} onChange={(e) => setName(e.target.value)} id="name" placeholder="name of reviewer" type="text" className="form-control" />
-          </div>
-          <div className="form-group col-4">
-            <label htmlFor="date">Date Hiked</label>
-            <input value={date} onChange={(e) => setDate(e.target.value)} id="date" placeholder="date hiked" type="text" className="form-control" />
-          </div>
+    <div className="container" style={{"width":"80%"}}>
+    <form action="">
+      <div className="row">
+        <div className="form-group col">
+          <label htmlFor="name">Name</label>
+          <input value={name} onChange={(e) => setName(e.target.value)} id="name" placeholder="Name of reviewer" type="text" className="form-control" />
         </div>
-        <div className="row pb-3">
-          <div className="form-group col-4">
-            <label htmlFor="title">Title</label>
-            <input value={title} onChange={(e) => setTitle(e.target.value)} id="title" placeholder="review title" type="text" className="form-control" />
-          </div>
-          <div className="form-group col-4">
-            <label htmlFor="description">Description</label>
-            <textarea value={description} onChange={(e) => setDescription(e.target.value)} id="description" placeholder="description of your trip - what do you want to share?" className="form-control" />
-          </div>
+        <div className="form-group col">
+          <label htmlFor="date">Date Hiked</label>
+          <input value={date} onChange={(e) => setDate(e.target.value)} id="date" placeholder="Date hiked" type="text" className="form-control" />
         </div>
-        <button onClick={handleSubmitReport} type="submit" className="btn btn-primary pt-2">Submit</button>
-      </form>
-      
-    </div>
+      </div>
+      <div className="row">
+        <div className="form-group col">
+          <label htmlFor="title">Title</label>
+          <input value={title} onChange={(e) => setTitle(e.target.value)} id="title" placeholder="Review title" type="text" className="form-control" />
+        </div>
+        <div className="form-group col">
+          <label htmlFor="description">Description</label>
+          <textarea value={description} onChange={(e) => setDescription(e.target.value)} id="description" placeholder="Description of your trip - what do you want to share?" className="form-control" />
+        </div>
+      </div>
+      <div className="d-flex justify-content-center my-3 px-4">
+        <button onClick={handleSubmitReport} type="submit" className="btn btn-secondary pt-2" style={{"width":"25%"}}>Submit</button>
+      </div>
+    </form>
+  </div>
   )
 }
 
