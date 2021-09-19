0fd09983accec236d232d84797761006fd28ae0b
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-09-18 23:01:38 -0700

    troubleshooting postgres db connection

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,14 @@
 const { Client } = require('pg');
 const db = process.env.NODE_ENV === 'test' ? 'hike_test' : 'hike_finder';
+const DATABASE_URL = process.env.DATABASE_URL;
+
+// const client = new Client({
+//   connectionString: `postgresql://localhost:5432/${db}`,
+// });
+let connection = process.env.DATABASE_URL || `postgresql://localhost:5432/${db}`;
 
 const client = new Client({
-  connectionString: `postgresql://localhost:5432/${db}`,
+  connectionString: connection,
 });
 
 client.connect();
