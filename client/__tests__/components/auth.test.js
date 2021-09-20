2a51b1315412adf84d98225246dafd0b5708aaf4
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-09-19 22:53:01 -0700

    completed basic render testing for components that dont require auth

---
+++ client/__tests__/components/auth.test.js
@@ -0,0 +1,39 @@
+// const app = require('../app.js');
+// const supertest = require('supertest');
+// const request = supertest(app.app);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from '../../src/routes/Home.jsx';
+import { useAuth0 } from '@auth0/auth0-react';
+require('dotenv').config();
+const sub = process.env.SUB;
+const email = process.env.EMAIL;
+// const request = require('supertest');
+// const app = require('../app.js');
+// const {useAuth0} = require('@auth0/auth0-react');
+
+const user = {
+  email: `${email}`,
+  email_verified: true,
+  sub: `${sub}`
+}
+
+jest.mock('@auth0/auth0-react');
+const mockedUseAuth0 = jest.mock(useAuth0);
+
+describe('testing auth', () => {
+  beforeEach(() => {
+    // Mock the Auth0 hook and make it return a logged in state
+    mockedUseAuth0.mockReturnValue({
+      isAuthenticated: true,
+      user,
+      logout: jest.fn(),
+      loginWithRedirect: jest.fn()
+    });
+  });
+
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render( < Home /> , div);
+  });
+})
