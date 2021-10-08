6971f249c07db557f12d658df2926786fdddefc8
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-10-08 13:21:23 -0700

    search terms and type posting to /search route

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import HikeFinder from '../apis/HikeFinder';
 
 const Search = () => {
+
+  const [searchTerms, setSearchTerms] = useState("");
+  const [searchType, setSearchType] = useState("");
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
+      console.log(searchTerms, searchType);
       //need to have this be a get to /search
-      const response = await HikeFinder.get
+      const response = await HikeFinder.post('/search', {
+        searchTerms,
+        searchType,
+      });
     } catch (err) {
       console.log(err);
     }
@@ -15,19 +23,20 @@ const Search = () => {
   return (
     <div className="input-group container d-flex justify-content-center my-3">
       <div className="form-outline" style={{ "width": "30%" }}>
-        <input type="search" className="form-control" placeholder="Search for a hike" />
+        <input type="text" className="form-control" placeholder="Search for a hike" onChange={(e) => setSearchTerms(e.target.value)}/>
       </div>
       <div>
         {/* This is the dropdown to select how to search */}
-        <select className="form-select" aria-label="Default select option">
-          <option defaultValue>How to Search</option>
-          <option value="1">Name</option>
-          <option value="2">Description</option>
-          <option value="3">Keywords</option>
-          <option value="4">Length</option>
+        {/* <label>Search By:</label> */}
+        <select className="form-select" aria-label="Default select option" onChange={(e) => setSearchType(e.target.value)}>
+          <option value="">Search By:</option>
+          <option value="name">Name</option>
+          <option value="description">Description</option>
+          <option value="keywords">Keywords</option>
+          <option value="length">Length</option>
         </select>
       </div>
-      <button type="button" className="btn btn-dark" style={{"width": "5%"}}>
+      <button type="button" className="btn btn-dark" style={{"width": "5%"}} onClick={handleSubmit}>
         <i className="fas fa-search"></i>
       </button>
     </div>
