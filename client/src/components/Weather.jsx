0aff14b871576c221a4bfe64297f3ee07e716019
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-10-14 14:36:04 -0700

    component setting weather info to state, passing daily detail via props to forecast component

--- client/src/components/Weather.jsx
+++ client/src/components/Weather.jsx
@@ -1,21 +1,29 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Forecast from './Forecast';
 require('dotenv').config();
 
-const Weather = ({latitude, longitude}) => {
-  
+
+const Weather = ({ latitude, longitude }) => {
+
   const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
   const lat = parseFloat(latitude);
   const lon = parseFloat(longitude);
+  const [weather, setWeather] = useState('');
+
   useEffect(() => {
-    axios.get(`https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${API_KEY}&include=minutely`)
-    .then(res => {
-      console.log('this is weather res', res);
-    });
-  })
+    const fetchWeather = async () => {
+      const response = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily/current?days=5&lat=${lat}&lon=${lon}&key=${API_KEY}`);
+      setWeather(response.data.data);
+    };
+    fetchWeather(); //eslint-disable-next-line
+  }, []);
+
   return (
-    <div className="row mb-2 d-flex justify-content-center">
-      This is lat and long: {latitude} and {longitude} at this location.
+    <div className="d-flex justify-content-center">
+      {!weather ? <h2>Loading weather...</h2> : weather.map((day, idx) => (
+        <Forecast key={idx} weatherDetail={day} />
+      ))}
     </div>
   )
 }
